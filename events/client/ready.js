555a691ed6f717c9eefae87e2fbcc6ec42fb8dd8
SpeedKenode <speedkenode@gmail.com>
2023-07-16 18:45:36 +0700

    add database

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,22 @@
-const { Client } = require("discord.js");
+const mongoose = require("mongoose");
+const mongodb = process.env.mongodburl;
 
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is online.`);
+
+        if (!mongodb) return;
+
+        await mongoose.connect(mongodb || "", {
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        if (mongoose.connect) {
+            console.log("The database is connecting")
+        }
     },
 };
